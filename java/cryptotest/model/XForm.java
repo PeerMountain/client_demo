4f0df46968b88b5a2520c805127df9c9ac18cf30
Christian BOdt <christian.bodt@coinplus.com>
2018-09-16 09:23:38 +0200

    -Add xform class

---
+++ java/cryptotest/model/XForm.java
@@ -0,0 +1,11 @@
+package cryptotest.model;
+
+import java.util.List;
+
+public class XForm {
+	List<String> fields_init;
+	
+	public XForm(List<String> fields_init) {
+		this.fields_init = fields_init;
+	}
+}
